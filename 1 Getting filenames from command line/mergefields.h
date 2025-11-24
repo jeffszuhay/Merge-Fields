e0bbd51784546b93e883e5cf0b09be66a2e4a550
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2025-11-24 14:44:31 -0600

    Add files via upload
    first step of C program

---
+++ 1 Getting filenames from command line/mergefields.h
@@ -0,0 +1,18 @@
+/*
+ * mergefields.h
+ */
+
+#ifndef _MERGEFIELDS_H_
+#define _MERGEFIELDS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>     // for getopt
+#include <sys/errno.h>  // for errno
+#include <stdbool.h>
+
+
+#endif
+
+/* eof */
